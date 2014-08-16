3ac96d49496c02407286a79584de518e57be39d4
Mohnish Jadwani <mail4mohnish@gmail.com>
2014-08-16 18:18:38 +0530

    adding the user who has created the idea

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,4 @@
 class Idea < ActiveRecord::Base
   has_many :comments
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140816102311) do
+ActiveRecord::Schema.define(version: 20140816123914) do
 
   create_table "comments", force: true do |t|
     t.string   "user_name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140816102311) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
